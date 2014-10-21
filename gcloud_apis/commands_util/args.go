00a17a5b88b7a29cd8e89c2e1936b4f1b301dcdd
John Asmuth <jasmuth@google.com>
2014-10-21 15:36:31 -0400

    Support for arg parsing.
    Change-Id: I2ebf196790e5c8f561a4ca9aa98b6a0d55cdca70

---
+++ gcloud_apis/commands_util/args.go
@@ -0,0 +1,77 @@
+package commands_util
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	NotFlagError = errors.New("args don't start with a flag")
+)
+
+/*
+Get a flag-value pair from the next one or two arguments, and return the args
+that are left over. One arg is consumed if it is of the form "--flag=value",
+two if it is of the form "--flag value".
+*/
+func extractFlagValue(args []string) (remainingArgs []string, flag, value string, err error) {
+	if len(args) == 0 {
+		panic("empty args")
+	}
+	if args[0] == "--" {
+		panic("trying to extract flag from remainder")
+	}
+	if !strings.HasPrefix(args[0], "--") {
+		err = NotFlagError
+		return
+	}
+	first_arg := args[0][2:]
+	if equals_index := strings.Index(first_arg, "="); equals_index != -1 {
+		flag = first_arg[:equals_index]
+		value = first_arg[equals_index+1:]
+		remainingArgs = args[1:]
+		return
+	}
+	if len(args) < 2 {
+		err = fmt.Errorf("unspecified flag value: %q", args[0])
+		return
+	}
+	flag = first_arg
+	value = args[1]
+	remainingArgs = args[2:]
+	return
+}
+
+/*
+Get all flag-value pairs from this args list, and return a new slice with the
+args that are left over.
+*/
+func ExtractFlagValues(args []string) (remainingArgs []string, values map[string]string, err error) {
+	values = map[string]string{}
+	for len(args) != 0 {
+		if args[0] == "--" {
+			// no more processing flags, the remaining args are positional now.
+			remainingArgs = append(remainingArgs, args[1:]...)
+			return
+		}
+
+		var leftOver []string
+		var f, v string
+		leftOver, f, v, err = extractFlagValue(args)
+		if err == NotFlagError {
+			remainingArgs = append(remainingArgs, args[0])
+			args = args[1:]
+			err = nil
+			continue
+		}
+		if err != nil {
+			return
+		}
+
+		values[f] = v
+		args = leftOver
+	}
+
+	return
+}
---
+++ gcloud_apis/commands_util/args_test.go
@@ -0,0 +1,192 @@
+package commands_util
+
+import (
+	"reflect"
+	"testing"
+)
+
+func considerCase(t *testing.T, foo func() map[string]interface{}, input interface{}, expected map[string]interface{}) {
+	errf := func(k string, v, e interface{}) {
+		t.Errorf("for %q, got %s %q, expected %q", input, k, v, e)
+	}
+
+	var result map[string]interface{}
+
+	defer func() {
+		panicMsg := recover()
+		expectedPanicMsg, ok := expected["panic"]
+		if panicMsg == nil != !ok {
+			errf("panic", panicMsg, expectedPanicMsg)
+			return
+		}
+		if !reflect.DeepEqual(panicMsg, expectedPanicMsg) {
+			errf("panic", panicMsg, expectedPanicMsg)
+			return
+		}
+	}()
+	result = foo()
+	if len(result) != len(expected) {
+		for key, expectedValue := range expected {
+			value, ok := result[key]
+			if !ok {
+				errf(key, value, expectedValue)
+			}
+		}
+	}
+	for key, value := range result {
+		expectedValue, ok := expected[key]
+		if !ok || !reflect.DeepEqual(value, expectedValue) {
+			errf(key, value, expectedValue)
+		}
+	}
+}
+
+func TestExtractFlagValue(t *testing.T) {
+
+	type Case struct {
+		Args     []string
+		Expected map[string]interface{}
+	}
+
+	cases := []Case{
+		{
+			Args: []string{"--foo", "bar", "baz"},
+			Expected: map[string]interface{}{
+				"flag":  "foo",
+				"value": "bar",
+				"args":  []string{"baz"},
+			},
+		},
+		{
+			Args: []string{"--foo=bar", "baz"},
+			Expected: map[string]interface{}{
+				"flag":  "foo",
+				"value": "bar",
+				"args":  []string{"baz"},
+			},
+		},
+		{
+			Args: []string{"--foo"},
+			Expected: map[string]interface{}{
+				"err": "unspecified flag value: \"--foo\"",
+			},
+		},
+		{
+			Args: []string{"x", "--foo"},
+			Expected: map[string]interface{}{
+				"err": "args don't start with a flag",
+			},
+		},
+		{
+			Args: []string{},
+			Expected: map[string]interface{}{
+				"panic": "empty args",
+			},
+		},
+	}
+	for _, fvt := range cases {
+		foo := func() map[string]interface{} {
+			args, flag, value, err := extractFlagValue(fvt.Args)
+			result := map[string]interface{}{}
+			if len(args) != 0 {
+				result["args"] = args
+			}
+			if flag != "" {
+				result["flag"] = flag
+			}
+			if value != "" {
+				result["value"] = value
+			}
+			if err != nil {
+				result["err"] = err.Error()
+			}
+			return result
+		}
+		considerCase(t, foo, fvt.Args, fvt.Expected)
+	}
+}
+
+func TestExtractFlagValues(t *testing.T) {
+	type TestCase struct {
+		Args           []string
+		Expected       map[string]interface{}
+		ExpectedValues map[string]string
+		ExpectedArgs   []string
+		ExpectedErr    error
+		ExpectedPanic  string
+	}
+
+	cases := []TestCase{
+		{
+			Args: []string{"--foo", "bar", "baz"},
+			Expected: map[string]interface{}{
+				"values": map[string]string{
+					"foo": "bar",
+				},
+				"args": []string{"baz"},
+			},
+		},
+		{
+			Args: []string{"baz", "--foo", "bar"},
+			Expected: map[string]interface{}{
+				"values": map[string]string{
+					"foo": "bar",
+				},
+				"args": []string{"baz"},
+			},
+		},
+		{
+			Args: []string{"--foo", "bar", "--baz", "qux"},
+			Expected: map[string]interface{}{
+				"values": map[string]string{
+					"foo": "bar",
+					"baz": "qux",
+				},
+			},
+		},
+		{
+			Args: []string{"--foo", "bar", "--", "--baz", "qux"},
+			Expected: map[string]interface{}{
+				"values": map[string]string{
+					"foo": "bar",
+				},
+				"args": []string{"--baz", "qux"},
+			},
+		},
+		{
+			Args: []string{"--foo", "bar", "--"},
+			Expected: map[string]interface{}{
+				"values": map[string]string{
+					"foo": "bar",
+				},
+			},
+		},
+		{
+			Args: []string{"x", "--foo", "bar", "y", "--", "--baz", "qux"},
+			Expected: map[string]interface{}{
+				"values": map[string]string{
+					"foo": "bar",
+				},
+				"args": []string{"x", "y", "--baz", "qux"},
+			},
+		},
+	}
+
+	for _, fvt := range cases {
+		foo := func() map[string]interface{} {
+			args, values, err := ExtractFlagValues(fvt.Args)
+			result := map[string]interface{}{}
+			if len(args) != 0 {
+				result["args"] = args
+			}
+			if len(values) != 0 {
+				result["values"] = values
+			}
+			if err != nil {
+				result["err"] = err.Error()
+			}
+			return result
+		}
+		considerCase(t, foo, fvt.Args, fvt.Expected)
+	}
+}
